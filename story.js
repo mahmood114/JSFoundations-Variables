bf69746b1edd5f9dedf54bd7c16ce0600309f918
mahmood114 <mahmoodalmohri@gmail.com>
2021-05-24 16:31:11 +0300

    Added and modified story.js file.

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "Sayyed Mahmood"
+let age = 22
+let hobby = "coding"
+
+console.log(`${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} exprience.`)
